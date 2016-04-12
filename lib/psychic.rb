23c31e5441666a60c593f405bfb74858537dce97
Nao Minami <south37777@gmail.com>
2016-04-12 14:09:51 +0900

    first commit

---
+++ lib/psychic.rb
@@ -0,0 +1,5 @@
+require "psychic/version"
+
+module Psychic
+  # Your code goes here...
+end
---
+++ spec/psychic_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Psychic do
+  it 'has a version number' do
+    expect(Psychic::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
