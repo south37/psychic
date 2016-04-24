08e40b7e8f747b3a51b1741e9232bdb9e311f49c
Nao Minami <south37777@gmail.com>
2016-04-24 17:44:42 +0900

    Add spec

--- spec/psychic_spec.rb
+++ spec/psychic_spec.rb
@@ -1,11 +1,47 @@
 require 'spec_helper'
 
 describe Psychic do
-  it 'has a version number' do
-    expect(Psychic::VERSION).not_to be nil
+  it 'does something useful' do
+    stub_request(:get, "www.example.com").
+      to_return(:body => original_response)
+    expect(Psychic.get("http://www.example.com").body).to eq result_response
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  def original_response
+    <<-EOF
+<!doctype html>
+<html>
+<head></head>
+<body>
+  <div id="main"></div>
+  <script>
+    var p = document.createElement("p");
+    var helloText = document.createTextNode("Hello, World!");
+    p.appendChild(helloText);
+    var main = document.getElementById("main");
+    main.appendChild(p);
+  </script>
+</body>
+    EOF
+  end
+
+  def result_response
+    <<-EOF
+<!doctype html>
+<html>
+<head></head>
+<body>
+  <div id="main">
+    <p>Hello, World!</p>
+  </div>
+  <script>
+    var p = document.createElement("p");
+    var helloText = document.createTextNode("Hello, World!");
+    p.appendChild(helloText);
+    var main = document.getElementById("main");
+    main.appendChild(p);
+  </script>
+</body>
+    EOF
   end
 end
